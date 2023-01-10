5bb1764a4210ba5a114573274c3a3daa43b9af11
Tyler Watson <tylerww91@gmail.com>
2023-01-09 21:02:50 -0800

    builds Footer component, adds footer to app.js, various css styling to Header.css Main.css Footer.css and Zodiac.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from './components/Header/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main.js';
 
 function App() {
@@ -10,25 +9,9 @@ function App() {
     <div className="App">
       <Header />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
-// <div className="App">
-//   <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <p>
-//       Edit <code>src/App.js</code> and save to reload.
-//     </p>
-//     <a
-//       className="App-link"
-//       href="https://reactjs.org"
-//       target="_blank"
-//       rel="noopener noreferrer"
-//     >
-//       Learn React
-//     </a>
-//   </header>
-// </div>
 
 export default App;
