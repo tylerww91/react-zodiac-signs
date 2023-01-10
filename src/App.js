0617ba6a017690e08804c7bec4843a0765ad17bd
Tyler Watson <tylerww91@gmail.com>
2023-01-09 19:56:27 -0800

    builds Header component, adds css styling to Header.css and Main.css, adds zodiac images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-// import Header from './components/Header';
+import Header from './components/Header/Header';
 // import Footer from './components/Footer';
 import Main from './components/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Main />
       {/* <Footer /> */}
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img className="header-img" src="../../astro-chart.gif" />
+      Whats your sign?
+      <img className="header-img" src="../../astro-chart.gif" />
+    </header>
+  );
+}
